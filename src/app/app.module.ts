996a9382946a4ceb05b08a26fb3237f6299b8dc5
florianlassalle <nairolfdix@gmail.com>
2019-09-30 16:33:49 +0200

    ajout des import pour formulaires

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +10,7 @@ import { PostsService } from './services/posts.service';
 import { NewPostComponent } from './new-post/new-post.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent, PostListItemComponent, PostListComponent, NewPostComponent ],
   bootstrap:    [ AppComponent ],
   providers: [PostsService]
